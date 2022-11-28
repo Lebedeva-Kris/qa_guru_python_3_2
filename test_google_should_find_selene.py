68b6d1125eaf49f7d02a6f8a6e564af313977a6c
Kristina <kris2015.lebedeva@gmail.com>
2022-11-28 22:02:56 +0300

    add negative case

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -2,6 +2,11 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-def test_body(open_browser):
+def test_body_positive(open_browser):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="rcnt"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in ...'))
+
+
+def test_body_negative(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('санагрварапупугандититини').press_enter()
+    browser.element('[aria-level="3"]').should(have.text('По запросу санагрварапупугандититини ничего не найдено.'))
