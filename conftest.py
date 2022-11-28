e3da24cc84302a4ced944aa7447246a2b556cc31
Kristina <kris2015.lebedeva@gmail.com>
2022-11-28 21:44:02 +0300

    refactor fixture

--- conftest.py
+++ conftest.py
@@ -9,13 +9,15 @@ def set_webdriver_config():
     browser.config.driver = webdriver.Chrome(ChromeDriverManager().install())
     return browser.config.driver
 
+
 @pytest.fixture()
 def set_browser_size(set_webdriver_config):
     browser.config.driver.maximize_window()
 
-@pytest.fixture(scope="session")
+
+@pytest.fixture()
 def open_browser(set_webdriver_config, set_browser_size):
     b = browser.open('https://google.com')
     print("Браузер открыт")
     yield b
-    print ("Браузер закрыт!")
+    print("Браузер закрыт!")
