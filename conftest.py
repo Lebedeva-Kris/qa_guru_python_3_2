1314e722c27c151ea72d4b7cb2a87f1533c128bd
Kristina <kris2015.lebedeva@gmail.com>
2022-11-28 21:29:02 +0300

    add config browser

--- conftest.py
+++ conftest.py
@@ -1,5 +1,14 @@
 import pytest
 from selene.support.shared import browser
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture()
+def set_webdriver_config():
+    browser.config.driver = webdriver.Chrome(ChromeDriverManager().install())
+    return browser.config.driver
+
 
 
 @pytest.fixture(scope="session")
@@ -8,3 +17,5 @@ def open_browser():
     print("Браузер открыт")
     yield b
     print ("Браузер закрыт!")
+
+@pytest.fixture(scope="session")
