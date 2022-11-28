7eb7bbcd4c6b130cc4a6b116d378e68694023b8e
Kristina <kris2015.lebedeva@gmail.com>
2022-11-28 21:38:51 +0300

    add size browser

--- conftest.py
+++ conftest.py
@@ -9,13 +9,13 @@ def set_webdriver_config():
     browser.config.driver = webdriver.Chrome(ChromeDriverManager().install())
     return browser.config.driver
 
-
+@pytest.fixture()
+def set_browser_size(set_webdriver_config):
+    browser.config.driver.maximize_window()
 
 @pytest.fixture(scope="session")
-def open_browser():
+def open_browser(set_webdriver_config, set_browser_size):
     b = browser.open('https://google.com')
     print("Браузер открыт")
     yield b
     print ("Браузер закрыт!")
-
-@pytest.fixture(scope="session")
